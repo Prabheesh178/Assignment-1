c6c0d48ac477c529b3265e809c2792cfcb4b9425
Prabheesh178 <85186879+Prabheesh178@users.noreply.github.com>
2025-05-28 03:08:33 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,21 @@
+
+#Task 1: Perform Basic Mathematical Operations
+x=input()
+y=input()
+#addition
+A=int(x)+int(y)
+print(A)
+#subtraction
+S=int(x)-int(y)
+print(S)
+#multiplication
+M=int(x)*int(y)
+print(M)
+#division
+D=int(x)/int(y)
+print(D)
+#Task 2: Create a Personalized Greeting
+f=input('ENTER YOUR FIRST NAME ')
+l=input('ENTER YOUE LAST NAMR ')
+Fullname=f+" "+ l 
+print("hello, " + Fullname +"! welcome to python program.")
